f53abeb6d962c4cc8dd0c32bdfa3e8014f036ed4
mickstorm <mstorm>
2015-03-20 09:48:45 -0700

    added routing for angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,26 @@
 
-
 'use strict';
 
 var angular = require('angular'); // That's right! We can just require angular as if we were in node
+require('angular-router-browserify')(angular);
+var HomeCtrl = require('./controllers/home'); // We can use our WelcomeCtrl.js as a module. Rainbows.
 
-var WelcomeCtrl = require('./controllers/home'); // We can use our WelcomeCtrl.js as a module. Rainbows.
+angular.module('prototype', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
 
-var app = angular.module('myApp', []);
+//END ROUTES
 
-app.controller('HomeCtrl', ['$scope', HomeCtrl]);
-
-
-
-//app.controller('HomeCtrl', ['$scope', HomeCtrl]);
-//app.controller('LoginCtrl', ['$scope', LoginCtrl]);
+//Controllers
+angular.module('prototype',[]).controller('HomeCtrl', ['$scope', HomeCtrl]);
 
