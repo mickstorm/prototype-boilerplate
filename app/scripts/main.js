84e1bcaeedb18a60bc702775ec6b35a870bb6d13
mickstorm <mstorm>
2015-03-20 09:27:51 -0700

    fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,14 @@
 
 
-var angular = require('angular');
-var HomeCtrl = require('./controllers/home');
-var HomeCtrl = require('./controllers/login');
+'use strict';
 
+var angular = require('angular'); // That's right! We can just require angular as if we were in node
 
-var app = angular.module('prototype',[
-	'ui.sortable',
-	'LocalStorageModule',
-	'ngRoute',
-	'ngSanitize'
-]).config(['$locationProvider', '$routeProvider', function (locationProvider, routeProvider) {
-  //locationProvider.html5Mode(true).hashPrefix('!');
-  //routes
-  routeProvider
-  .when('/', {
-    templateUrl: 'views/home.html',
-    controller: 'LoginCtrl'
- });
+var WelcomeCtrl = require('./controllers/home'); // We can use our WelcomeCtrl.js as a module. Rainbows.
 
+var app = angular.module('myApp', []);
 
-
-
-
+app.controller('HomeCtrl', ['$scope', HomeCtrl]);
 
 
 
